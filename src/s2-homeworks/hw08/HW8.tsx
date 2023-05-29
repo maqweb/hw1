2da815815f124e18cd4f79d78ec2d3eb16a04092
Vitaly Balikhin <balikhinv@bk.ru>
2023-05-30 00:43:40 +0300

    hw8

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -7,7 +7,6 @@ import User from './User'
 
 /*
 * 1 - дописать типы и логику (сортировка по имени, фильтрация по совершеннолетию) homeWorkReducer, проверить тестом
-* 2 - дописать компоненту User
 * 3 - сделать стили в соответствии с дизайном
 * */
 
@@ -18,13 +17,11 @@ export type UserType = {
 }
 
 const initialPeople: UserType[] = [
-    // студенты могут поменять имя/возраст/количество объектов, _id должны быть целочисленные
-    {_id: 0, name: 'Кот', age: 3},
-    {_id: 1, name: 'Александр', age: 66},
-    {_id: 2, name: 'Коля', age: 16},
-    {_id: 3, name: 'Виктор', age: 44},
-    {_id: 4, name: 'Дмитрий', age: 40},
-    {_id: 5, name: 'Ирина', age: 55},
+    {_id: 0, name: 'Cat', age: 3},
+    {_id: 1, name: 'Jhon Snow', age: 23},
+    {_id: 2, name: 'Bob', age: 16},
+    {_id: 3, name: 'Ivan the terrible', age: 44},
+    {_id: 4, name: 'Maya', age: 40},
 ]
 
 const HW8 = () => {
@@ -36,20 +33,20 @@ const HW8 = () => {
     const sortUp = () => {
         setPeople(
             homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'})
-        ) // в алфавитном порядке a.name > b.name
+        )
         setCurrentSort('up')
     }
 
     const sortDown = () => {
         setPeople(
             homeWorkReducer(initialPeople, {type: 'sort', payload: 'down'})
-        ) // в обратном порядке a.name < b.name}
+        )
         setCurrentSort('down')
     }
     const check18 = () => {
         setPeople(
             homeWorkReducer(initialPeople, {type: 'check', payload: 18})
-        ) // совершеннолетние
+        )
         setCurrentSort('18')
     }
 
@@ -63,6 +60,7 @@ const HW8 = () => {
                             id={'hw8-button-up'}
                             onClick={sortUp}
                             xType={currentSort === 'up' ? '' : 'secondary'}
+                            style={{marginRight: '10px'}}
                         >
                             Sort up
                         </SuperButton>
@@ -70,6 +68,7 @@ const HW8 = () => {
                             id={'hw8-button-down'}
                             onClick={sortDown}
                             xType={currentSort === 'down' ? '' : 'secondary'}
+                            style={{marginRight: '10px'}}
                         >
                             Sort down
                         </SuperButton>
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,32 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8'
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case 'sort': {
+            if (action.payload === 'up') {
+                return state.sort((a, b) => {
+                    if (a.name.toLowerCase() > b.name.toLowerCase()) {
+                        return 1
+                    } else {
+                        return -1
+                    }
+                })
+            } else {
+                return state.sort((a, b) => {
+                    if (a.name.toLowerCase() < b.name.toLowerCase()) {
+                        return 1
+                    } else {
+                        return -1
+                    }
+                })
+            }
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(user => user.age >= 18)
         }
         default:
             return state
